6a3edeae88e8fe39b7039bfe0a73f6baee3362e2
dh905 <thebigpig905@gmail.com>
2024-10-26 14:17:59 -0600

    NA

--- Scripts/Game/player.gd
+++ Scripts/Game/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 var held: int = 0
 var specials
 var dir = Vector2(0 , 0)
+var ext =Vector2 (1 , 1)
 var speed = 150
 var size: Vector2 = Vector2(16 , 30)
 var plr:int
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	velocity = dir * speed
+	velocity = dir * speed * ext
 	move_and_slide()
 
 
