7681cd94094ed3b0caeb0c82bf49758d60d8c736
dh905 <thebigpig905@gmail.com>
2024-11-04 06:56:00 -0700

    sizes

--- Scripts/Game/obstacle.gd
+++ Scripts/Game/obstacle.gd
@@ -2,6 +2,10 @@ extends Sprite2D
 var speed
 var type
 
+func _ready() -> void:
+	var n = (randf() * 2) + 1
+	scale = Vector2(2 * n , 2 * n)
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.get_parent().get_parent().get_parent().sheilded == false:
 		body.hurt()
